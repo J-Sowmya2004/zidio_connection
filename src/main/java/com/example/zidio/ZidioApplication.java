2db7107dcaf76f24a353434dbf55a85976c29efd
J-Sowmya2004 <sowmyajayanthi2004@gmail.com>
2025-08-27 15:55:37 +0530

    Add files via upload

---
+++ src/main/java/com/example/zidio/ZidioApplication.java
@@ -0,0 +1,14 @@
+package com.example.zidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ZidioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZidioApplication.class, args);
+	}
+
+}
